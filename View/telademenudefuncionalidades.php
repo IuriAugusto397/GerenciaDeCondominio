bf63148e9c99e94fb2ece3282893a94986f04494
JhulyanoSoares <jhulyano_soares@hotmail.com>
2023-04-14 18:26:30 -0300

    adicionando botao logout

--- View/telademenudefuncionalidades.php
+++ View/telademenudefuncionalidades.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" >-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" >
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu de funcionalidades</title>
@@ -11,11 +11,11 @@
             background-color: rgb(50, 50, 40);
             text-align: center;
             position: absolute;
-            width: 700px;
+            width: 800px;
             height: 500px;
             top: 50%;
             left: 50%;
-            margin-left: -350px;
+            margin-left: -400px;
             margin-top: -250px;
         }
         h1,h3{
@@ -36,6 +36,19 @@
             background-color: chartreuse;
             cursor: pointer;
         }
+        .absolute {
+            font-size: 10px;
+            background-color: red;
+            height: 30px;
+            width: 60px;
+            position: absolute;
+            top: 10px;
+            right: 10px;
+        }
+        .absolute:hover{
+            background-color: purple;
+            cursor: pointer;
+        }
     </style>
 </head>
 <body>
@@ -45,7 +58,7 @@
         <a href="cadastroMoradores.php"><button>Cadastrar novo morador</button></a><br/><br/>
         <a href="multas.php"><button>Aplicar/Notificar multa</button></a><br/><br/>
         <a href="telanotif.php"><button>Fazer notificações gerais</button></a><br/><br/>
-        <a href="trocalogin.php"><button>Alterar login e senha</button></a>
+        <a href="trocalogin.php"><button>Alterar login e senha</button></a><button class="absolute" id="logout">Logout</button>
     </div>
 </body>
 </html>
